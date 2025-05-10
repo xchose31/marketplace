228f145ac2bd988a511dbc9e3145d7ecc67ebe68
xchose31 <rte3001@vk.com>
2025-05-10 14:04:14 +0300

    добавлено создание товара от магазина

---
+++ forms/product_creating_form.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FileField, IntegerField
+from wtforms.validators import DataRequired
+
+
+class ProductForm(FlaskForm):
+    name = StringField('Название товара', validators=[DataRequired()])
+    description = StringField('Описание товара', validators=[DataRequired()])
+    category = StringField("Категория товара", validators=[DataRequired()])
+    logo = FileField('Картинка товара', validators=[DataRequired()])
+    stock_quantity = IntegerField('Количество товара', validators=[DataRequired()])
+    price = IntegerField("Цена товара за штуку", validators=[DataRequired()])
+    submit = SubmitField('Создать товар')
